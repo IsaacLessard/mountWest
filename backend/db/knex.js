01275ec70d77fbab75bc822852f6011a541afe74
Isaac Lessard <IsaacL@Isaacs-MacBook-Air-2.local>
2016-01-22 12:47:01 -0700

    working on routes

---
+++ backend/db/knex.js
@@ -0,0 +1,4 @@
+var knex = require('knex')({
+  client: 'pg',
+  connection: 'postgres://wkhhmwlczmhzhi:fo2Gmf0vYX4HuGGOoASS7XdSAS@ec2-54-83-61-45.compute-1.amazonaws.com:5432/dfofh46ecrdlj1?ssl=true'
+});
