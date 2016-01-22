27ad39c5dd1abe05ebd98f7846dfd24796a37d7f
Isaac Lessard <IsaacL@Isaacs-MacBook-Air-2.local>
2016-01-22 14:11:32 -0700

    :beginner: routes and events

---
+++ backend/routes/events.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var router = express.Router();
+var knex = require('../db/knex');
+var app = express();
+
+app.get('/', function (req, res, next) {
+  res.json('events')
+})
